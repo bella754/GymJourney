9d17f8adb7a2b544889eaaaabd20da5171ad00b3
Ahmed <toumi@tu-berlin.de>
2024-06-19 02:04:45 +0200

    fix: Navigation buttons made visible

--- common/components/Clockbutton.tsx
+++ common/components/Clockbutton.tsx
@@ -2,17 +2,24 @@ import { Component } from "uix/components/Component.ts";
 
 @template<Props>((props) => (
     <div>
-         <button>
-		 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-</svg>
-
-
-
-    </button>
+        <button>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+            </svg>
+        </button>
     </div>
 ))
-
 @style(css`
     button {
         display: flex;
@@ -20,10 +27,15 @@ import { Component } from "uix/components/Component.ts";
         align-items: center;
         width: 50px;
         height: 50px;
-        border: 2px solid;
+        border: 2px solid black;
         border-radius: 8px;
         background-color: white;
     }
-`)
 
+    svg {
+        width: 42px;
+        height: 42px;
+        stroke: black; 
+    }
+`)
 export class Clockbutton extends Component<Props> {}
