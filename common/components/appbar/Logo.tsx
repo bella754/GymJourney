2c826baa6ecdb9cc8cceca992c574e8cc2297a1e
Alan Ostaszewski <alan.ostaszewski@icloud.com>
2024-06-23 17:05:18 +0200

    feat: appbar component

---
+++ common/components/appbar/Logo.tsx
@@ -0,0 +1,20 @@
+import { Component } from "uix/components/Component.ts";
+
+@template(() => (
+  <div class="container">
+    <img src="/@uix/src/common/res/Logowithtitle.jpg" />
+    <p>GymJourney</p>
+  </div>
+))
+@style(css`
+  .container {
+    display: flex;
+    gap: 10px;
+  }
+
+  img {
+    width: 50px;
+    height: 50px;
+  }
+`)
+export class Logo extends Component {}
