56a517f2b398dfb594b252b4b69505429f0184ed
Alan Ostaszewski <alan.ostaszewski@icloud.com>
2024-06-24 10:41:30 +0200

    chore: delete unused buttons

--- common/components/Button.tsx
+++ common/components/Button.tsx
@@ -29,3 +29,68 @@ type Props = {};
   }
 `)
 export class Button extends Component<Props> {}
+
+// secondary button
+/* import { Component } from "uix/components/Component.ts";
+
+@template<Props>((props) => (
+	<div>
+	<button uix-title={'but'}>Log In</button>
+	</div>
+))
+
+@style(css`
+button{
+	width: 311px;
+	height: 63px;
+	font-size: 22px;
+	border-radius: 8px;
+	border: #F0F0F0;
+	box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+	color: black;
+	background-color: #F0F0F0;
+}
+
+button:hover {background-color: #F0F0E8}
+
+button:active {
+	background-color: #F0F0F0;
+	box-shadow: 0 3px lightgrey;
+	transform: translateY(4px);
+}
+`)
+
+
+export class Buttonsecondary extends Component <Props>{} */
+
+// link button
+/* import { Component } from "uix/components/Component.ts";
+
+@template<Props>((props) => (
+	<div>
+	<button>Forgot Password?</button>
+	</div>
+))
+
+@style(css`
+button{
+	width: 100px;
+	height: 16px;
+	border: none;
+	font-size: 9px;
+	color: #4E75FC;
+	background-color: transparent;
+	text-decoration: underline;
+}
+
+button:hover{
+	color: #4E75EE;
+}
+
+button:active {
+	color: #4E75EE;
+}
+`)
+
+
+export class Buttontransparent extends Component <Props>{} */
