ee37ea15443da13cbc3725cb6374c1cf05255274
Alan Ostaszewski <alan.ostaszewski@icloud.com>
2024-06-26 13:53:39 +0200

    feat: get user history data from backend

--- common/routes/history/Index.tsx
+++ common/routes/history/Index.tsx
@@ -1,6 +1,8 @@
 import { AppBar } from "../../components/appbar/AppBar.tsx";
 import { Component } from "uix/components/Component.ts";
 import { BottomBar } from "common/components/bottombar/BottomBar.tsx";
+import { userData } from "../../../backend/data.ts";
+import { Card } from "common/components/card/Card.tsx";
 
 type Props = {};
 
@@ -10,6 +12,30 @@ type Props = {};
     <div style="margin: 10px auto; display: flex; justify-content: center; align-items: center; max-width: 600px; width: 100%; height: 100%;">
       <div style="display: flex; flex-direction: column; align-items: center;">
         <h2>History</h2>
+        {/* @ts-ignore */}
+
+        {userData[0].trainingsHistory.$.map(
+          (training: {
+            date: string;
+            start: string;
+            end: string;
+            duration: string;
+            training: string;
+            difficulty: string;
+          }) => (
+            <Card>
+              <div>
+                <h3>{training.date}</h3>
+                <p>
+                  {training.start} - {training.end}
+                </p>
+                <p>Duration: {training.duration}</p>
+                <p>Training: {training.training}</p>
+                <p>Difficulty: {training.difficulty}</p>
+              </div>
+            </Card>
+          )
+        )}
       </div>
     </div>
     <BottomBar />
