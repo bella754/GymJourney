4a966330a9533f7770e15832ce93c05917ae8b56
Alan Ostaszewski <alan.ostaszewski@icloud.com>
2024-07-02 21:47:23 +0200

    feat: settings page

--- backend/data/user.model.ts
+++ backend/data/user.model.ts
@@ -1,53 +1,67 @@
 import { Datex } from "unyt_core/mod.ts";
-//Datex.Runtime.OPTIONS.PROTECT_POINTERS = true; // Privatize pointers to their users
+/* Datex.Runtime.OPTIONS.PROTECT_POINTERS = true; // Privatize pointers to their users */
 
 interface IUser {
-    name: string,
-    gender: 'm' | 'f' | 'd' | 'not specified',
-    age: number,
-    height: number,
-    weight: number,
-    ethnicity: "Asian" | "Black" | "Hispanic" | "White" | "Native American" | "Other" |'not specified',
-    experience: "beginner" | "intermediate" | "advanced" | "expert" | "not specified",
-    gym: string,
-    //avatarUrl: string
-};
+  name: string;
+  gender: "m" | "f" | "d" | "not specified";
+  age: number;
+  height: number;
+  weight: number;
+  ethnicity:
+    | "Asian"
+    | "Black"
+    | "Hispanic"
+    | "White"
+    | "Native American"
+    | "Other"
+    | "not specified";
+  experience:
+    | "beginner"
+    | "intermediate"
+    | "advanced"
+    | "expert"
+    | "not specified";
+  gym: string;
+  //avatarUrl: string
+}
 
 const users = $$({} as Record<string, IUser[]>);
 
 // create
 export function createUser() {
-    const user = datex.meta.caller.main.toString();
-    console.log(`Creating databse entry for ${user}.`);
-    users[user] = [{
-        name: "Anonymous",
-        gender: "not specified",
-        age: 0,
-        height: 0,
-        weight: 0,
-        ethnicity: 'not specified', 
-        experience: "not specified",
-        gym: "not specified"
-    }];
+  const user = datex.meta.caller.main.toString();
+  console.log(`Creating databse entry for ${user}.`);
+  users[user] = [
+    {
+      name: "Anonymous",
+      gender: "not specified",
+      age: 0,
+      height: 0,
+      weight: 0,
+      ethnicity: "not specified",
+      experience: "not specified",
+      gym: "not specified",
+    },
+  ];
 
-    return users[user]
+  return users[user];
 }
 
 // read
 export function getUser() {
-	const user = datex.meta.caller.main.toString();
+  const user = datex.meta.caller.main.toString();
 
-	if (!(user in users)) {
-		return createUser(); 
-	}
+  if (!(user in users)) {
+    return createUser();
+  }
 
-    return users[user]
+  return users[user];
 }
 
 // update
 export function updateUser(data: Partial<IUser>) {
-    const user = datex.meta.caller.main.toString();
-    console.log(`Updating databse entry for ${user}.`);
-    users[user] = users[user].map(u => ({...u, ...data}));
-    return users[user];
+  const user = datex.meta.caller.main.toString();
+  console.log(`Updating databse entry for ${user}.`);
+  users[user] = users[user].map((u) => ({ ...u, ...data }));
+  return users[user];
 }
--- common/routes/settings/Index.tsx
+++ common/routes/settings/Index.tsx
@@ -1,53 +1,69 @@
 import { AppBar } from "../../components/appbar/AppBar.tsx";
 import { Component } from "uix/components/Component.ts";
 import { BottomBar } from "common/components/bottombar/BottomBar.tsx";
-import { createUser, getUser } from "../../../backend/data/user.model.ts";
+import { getUser, updateUser } from "../../../backend/data/user.model.ts";
+import { Button } from "common/components/Button.tsx";
 
 type Props = {};
 
-const [ user ] = await createUser();
+const [user] = await getUser();
 
-/* const userName = $$(user.name);
+const userName = $$(user.name);
 const userGender = $$(user.gender);
 const userAge = $$(user.age);
 const userHeight = $$(user.height);
 const userWeight = $$(user.weight);
 const userEthnicity = $$(user.ethnicity);
 const userExperience = $$(user.experience);
-const userGym = $$(user.gym); */
+const userGym = $$(user.gym);
 
-
-@template<Props>(async () => (
+@template<Props>(() => (
   <div>
     <AppBar />
     <div style="margin: 10px auto; display: flex; justify-content: center; align-items: center; max-width: 600px; width: 100%; height: 100%;">
       <div style="display: flex; flex-direction: column; align-items: center;">
-        <h1>Settings</h2>
-        
+        <h1>Settings</h1>
+
         <hr />
 
         <h2>User Information</h2>
-        {/* <h3>{user.name}</h3>
+        <h3>{user.name}</h3>
         <h3>{user.gender}</h3>
         <h3>{user.age}</h3>
         <h3>{user.height}</h3>
         <h3>{user.weight}</h3>
         <h3>{user.ethnicity}</h3>
         <h3>{user.experience}</h3>
-        <h3>{user.gym}</h3> */}
+        <h3>{user.gym}</h3>
 
         <hr />
 
         <h2>Edit User Information</h2>
-        {/* <input type="text" placeholder="Name" value={userName} />
+        <input type="text" placeholder="Name" value={userName} />
         <input type="text" placeholder="Gender" value={userGender} />
         <input type="text" placeholder="Age" value={userAge} />
         <input type="text" placeholder="Height" value={userHeight} />
         <input type="text" placeholder="Weight" value={userWeight} />
         <input type="text" placeholder="Ethnicity" value={userEthnicity} />
         <input type="text" placeholder="Experience" value={userExperience} />
-        <input type="text" placeholder="Gym" value={userGym} /> */}
+        <input type="text" placeholder="Gym" value={userGym} />
 
+        <Button
+          onclick={() =>
+            updateUser({
+              name: userName,
+              gender: userGender,
+              age: userAge,
+              height: userHeight,
+              weight: userWeight,
+              ethnicity: userEthnicity,
+              experience: userExperience,
+              gym: userGym,
+            })
+          }
+        >
+          Save
+        </Button>
       </div>
     </div>
     <BottomBar />
