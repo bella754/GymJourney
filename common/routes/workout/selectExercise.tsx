cad9060a4ae54e1cbcff9196c57e20d0215ebd3c
Alan Ostaszewski <alan.ostaszewski@icloud.com>
2024-07-08 21:36:53 +0200

    feat: search for exercises (WIP)
    Co-authored-by: grandvegas <grandvegas@users.noreply.github.com>
    Co-authored-by: ariamahin <ariamahin@users.noreply.github.com>

--- common/routes/workout/selectExercise.tsx
+++ common/routes/workout/selectExercise.tsx
@@ -1,32 +1,34 @@
-import { AppBar } from '../../components/appbar/AppBar.tsx';
-import { Component } from 'uix/components/Component.ts';
-import { BottomBar } from 'common/components/bottombar/BottomBar.tsx';
-import { Button } from 'common/components/Button.tsx';
-import { getExercises, getWorkoutById, addExercisesToWorkout } from 'backend/api/training/training.crud.ts';
+import { AppBar } from '../../components/appbar/AppBar.tsx'
+import { Component } from 'uix/components/Component.ts'
+import { BottomBar } from 'common/components/bottombar/BottomBar.tsx'
+import { Button } from 'common/components/Button.tsx'
+import { getExercises, getWorkoutById, addExercisesToWorkout } from 'backend/api/training/training.crud.ts'
 
 type Props = {
   id: string
-};
+}
 
 @template<Props>(async (_, { id }) => {
-  const allExercises = await getExercises();
-  const selectedExercises = $$([]);
+  const allExercises = await getExercises()
+  const selectedExercises = $$([])
+
+  const search = $$('')
 
   const handleCheckboxChange = (exercise, isChecked) => {
     if (isChecked) {
-      selectedExercises.push(exercise);
+      selectedExercises.push(exercise)
     } else {
-      const index = selectedExercises.findIndex(e => e.name === exercise.name);
+      const index = selectedExercises.findIndex((e) => e.name === exercise.name)
       if (index > -1) {
-        selectedExercises.splice(index, 1);
+        selectedExercises.splice(index, 1)
       }
     }
-  };
+  }
 
   const saveExercises = async () => {
-    await addExercisesToWorkout(id, selectedExercises);
-    window.location.href = `/createWorkout/${id}`;
-  };
+    await addExercisesToWorkout(id, selectedExercises)
+    window.location.href = `/createWorkout/${id}`
+  }
 
   return (
     <div>
@@ -34,22 +36,25 @@ type Props = {
       <div style="margin: 10px auto; display: flex; justify-content: center; align-items: center; max-width: 600px; width: 100%; height: 100%;">
         <div style="display: flex; flex-direction: column; align-items: center; gap: 20px; width: 100%">
           <h2>Übungen auswählen</h2>
-          {allExercises.map((exercise) => (
-            <div style="margin-bottom: 20px;">
-              <input type="checkbox" id={exercise.name} value={exercise.name} onchange={(e) => handleCheckboxChange(exercise, e.target.checked)} />
-              <label for={exercise.name}>{exercise.name}</label>
-            </div>
-          ))}
+          <input type="text" placeholder="Suche" style="margin-bottom: 20px; padding: 10px; width: 100%;" value={search} />
+          {allExercises
+            .filter((exercise) => exercise.name.includes(search))
+            .map((exercise) => (
+              <div style="margin-bottom: 20px;">
+                <input type="checkbox" id={exercise.name} value={exercise.name} onchange={(e) => handleCheckboxChange(exercise, e.target.checked)} />
+                <label for={exercise.name}>{exercise.name}</label>
+              </div>
+            ))}
           <Button onclick={() => window.history.back()}>Zurück</Button>
         </div>
         <Button onclick={saveExercises}>Speichern</Button>
       </div>
       <BottomBar />
     </div>
-  );
+  )
 })
 @style(css`
-  input[type="checkbox"] {
+  input[type='checkbox'] {
     margin-right: 10px;
   }
   label {
