6971a8863cba9128a7b8da3aa8ac757fdc532791
Alan Ostaszewski <alan.ostaszewski@icloud.com>
2024-07-10 18:56:45 +0200

    feat: lvl-bar

---
+++ backend/api/lvl/lvl.interface.ts
@@ -0,0 +1,16 @@
+export interface ILvl {
+  trainingVolume: number
+  trainingDuration: number
+  xp: number
+}
+
+export interface IBarOutput {
+  value: number
+  max: number
+}
+
+export interface ILvlOutput {
+  lvl: number
+  xp: number
+  xpMax: number
+}
